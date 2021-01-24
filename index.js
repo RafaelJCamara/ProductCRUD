c2819756c154bfa6e360fe760f6952884633ea98
rafaeljcamara <rafaelcamarac@gmail.com>
2021-01-24 12:29:31 +0000

    Add REST functionalities (index, details and creating products)

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const Product = require("./models/product");
+const methodOverride = require('method-override');
+const { Console } = require("console");
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -16,13 +18,39 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 
+//Showcase products
 app.get("/products", async (req,res)=>{
     const products = await Product.find({});
-    res.send("We have all the products");
+    res.render("index",{products});
 });
 
+//Save the new product
+app.post("/products", async (req,res)=>{
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect("/products");
+});
+
+//Create new product
+app.get("/products/new", (req,res)=>{
+    res.render("new");
+});
+
+//Show product details
+app.get("/products/:id", async (req,res)=>{
+    const {id} = req.params;
+    try {
+        const product = await Product.findById(id);
+        res.render("show",{product});
+    } catch (error) {
+        console.log("There was an error...");
+        console.log(error);
+    }
+});
 
 app.listen(3000,()=>{
     console.log("Listening on port 3000...");
